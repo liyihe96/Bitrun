e8867f5c589b175bb07f9453201ca071f9b7bee9
Yihe Li <liyihe96@gmail.com>
2014-11-08 00:31:33 -0800

    This is the true initial commit

---
+++ Bitrun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bitrun
+//
+//  Created by Yihe Li on 11/8/14.
+//  Copyright (c) 2014 Yihe Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
